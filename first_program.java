b719f7301a0beacb1e351c6cf2acf42a422b933b
dharmadevipurushothaman <dharmadevi123cool@gmail.com>
2019-12-26 13:57:23 +0000

    change java program

--- first_program.java
+++ first_program.java
@@ -5,6 +5,6 @@ public class first_program {
       int num1 = 5, num2 = 15, sum;
       sum = num1 + num2;
 
-      System.out.printn("Sum of these numbers: "+sum);
+      System.out.println("Sum of these numbers: "+sum);
    }
 }
